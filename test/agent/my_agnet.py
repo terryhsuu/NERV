eaa3c532e87f1b8015008a12d5edf103a965207d
曾柏瑜 <abcdefg35216874@gmail.com>
2020-12-13 00:25:08 +0800

    the previous file has some problem

---
+++ test/agent/my_agnet.py
@@ -0,0 +1,8 @@
+from my_agent import *
+class MyAgent(useful_step):
+    def __init__(self):
+        super().__init__():
+
+    for i in range(64):
+        pass
+        
---
+++ test/test.py
@@ -0,0 +1,4 @@
+a = [[ i + j*8 for i in range(8)] for j in range(8)]
+for i in a:
+    print(i)
+print(6.3%3)
